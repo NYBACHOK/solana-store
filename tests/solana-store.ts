f91d5bf0c1c4fdee51e0f306aa31e4f48557ba27
NYBACHOK <nybachokua@gmail.com>
2025-02-06 12:51:23 +0200

    test1

--- tests/solana-store.ts
+++ tests/solana-store.ts
@@ -1,4 +1,5 @@
 import * as anchor from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaStore } from "../target/types/solana_store";
 import { assert } from "chai";
@@ -8,37 +9,41 @@ anchor.web3.SendTransactionError
 
 describe("solana-store", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env(); 
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaStore as Program<SolanaStore>;
-  const addr = "c312fa79e86049a12a9924a86283cf0b";
-
+ 
   it("Create PDA account", async () => {
+    const addr = "c312fa79e86049a12a9924a86283cf0b";
+
     // Create new PDA account then query it and check limit is set correctly
     await program.methods.initialize(addr, 10).rpc();
-    const keys = (await program.account.apiKeyAccount.all())[0];
+    
+    const messageAccount = (await program.account.apiKeyAccount.all())[0];
      
-    assert(keys.account.key == addr);
-    assert(keys.account.limit == 10);
+    assert(messageAccount.account.key == addr);
+    assert(messageAccount.account.limit == 10);
   });
 
-  it("Edit PDA account - OK", async () => {
-    let tx = new Transaction();
+  // it("Edit PDA account - OK", async () => {
+  //   const addr = "F3776A0186D043CEAE8FE5A5992D30D1";
+
+  //   let tx = new Transaction();
+
+  //   tx.add( await program.methods.initialize(addr, 10).instruction());
+  //   tx.add(await program.methods.editLimit(addr, 20).instruction());
+  //   // await program.methods.initialize(addr, 10).rpc();
+  //   // await program.methods.editLimit(addr, 20).rpc();
+
+  //   // await program.provider.send(tx);
 
   
-    tx.add( await program.methods.initialize(addr, 10).instruction());
-    tx.add(await program.methods.editLimit(addr, 20).instruction());
-    // await program.methods.initialize(addr, 10).rpc();
-    // await program.methods.editLimit(addr, 20).rpc();
+  //   await program.provider.sendAndConfirm(tx);
 
-    // await program.provider.send(tx);
-
-  
-    await program.provider.sendAndConfirm(tx);
-
-    // const keys = (await program.account.apiKeyAccount.all())[0];
+  //   // const keys = (await program.account.apiKeyAccount.all())[0];
      
-    // assert(keys.account.key == addr);
-    // assert(keys.account.limit == 20);
-  });
+  //   // assert(keys.account.key == addr);
+  //   // assert(keys.account.limit == 20);
+  // });
 });
